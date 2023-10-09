4524d703a13a755db593af88e0805892a5920434
Maiihess <maiaraangelii@gmail.com>
2023-10-09 18:37:11 -0300

    atualização 09/10

---
+++ public/pesquisa.php
@@ -0,0 +1,16 @@
+<?php
+
+include "db.php";
+
+$aconteceu = $_POST["aconteceu"];
+$outras_vezes = $_POST["outras_vezes"];
+$tempo_aconteceu = $_POST["tempo_aconteceu"];
+$problema_saude = $_POST["problema_saude"];
+$quais = $_POST["quais"];
+$medicacao = $_POST["medicacao"];
+$ultima_medicacao = $_POST["ultima_medicacao"];
+$quais_medicacao = $_POST["quais_medicacao"];
+$alergico = $_POST["alergico"];
+$alergia = $_POST["alergia"];
+$ingeriu = $_POST["ingeriu"];
+$horario_ingeriu = $_POST["horario_ingeriu"];

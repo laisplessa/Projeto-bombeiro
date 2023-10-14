573733af37d3fbc1ae1a98bb7df5ec1719e20648
Maiihess <maiaraangelii@gmail.com>
2023-10-14 19:12:28 -0300

    ATUALIZAÇÃO

--- public/ficha.php
+++ public/ficha.php
@@ -8,18 +8,28 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 
 session_start();
 
-$VARIAVEL_QUE_VC_QUISER = $_POST["NAME DO INPUT"]
-$outrasVezes = $_POST["outrasVezes"];
-$cpf = $_POST["cpf"];
-$senha = $_POST["senha"];
+$VARIAVEL_QUE_VC_QUISER = $_POST["NAME DO INPUT"];
+$outrasVezes = $_POST["outras_vezes"];
+$quantoTempo = $_POST["quanto_tempo"];
+$problemaSaude = $_POST["problema_saude"];
+$quaisProblemas = $_POST["quais_problemas"];
+$medicacao = $_POST["medicacao"];
+$horaMedicacao  = $_POST["hora_medicacao"];
+$quaisMedicacoes = $_POST["quais_medicacoes"];
+$alergico = $_POST["alergico"];
+$alergia = $_POST["alergia"];
+$ingeriu = $_POST["ingeriu"];
+$ingeriuHorario = $_POST["ingeriu_horario"];
+
 
 $mensagemErro = "";
 
-if ($outrasVezes == null) {
-    $mensagemErro = "O campo Aconteceu outras vezes é obrigatório \n";
-}
 if ($outroCampo == null) {
-    $mensagemErro = "O campo Outro campo é obrigatório \n";
+  $mensagemErro = "O campo Outro campo é obrigatório \n";
+}
+
+if ($outrasVezes == null) {
+    $mensagemErro = "O campo aconteceu outras vezes é obrigatório \n";
 }
 
 if ($mensagemErro != "") {
@@ -33,26 +43,6 @@ if ($mensagemErro != "") {
     $sql = "INSERT INTO usuario (CAMPO DA TABELA QUE EU QUERO, cpf, senha) VALUES ('$VARIAVEL_QUE_EU_QUISER', '$cpf', '$senha')";
     $run_query = mysqli_query($con, $sql);
 
-    if ($run_query) {
-
-      $_SESSION['cpf'] = $cpf;
-
-      $data = array("erro" => false, "mensagem" => "Usuário cadastrado com sucesso");
-      header("Content-Type: application/json");
-      echo json_encode($data);
-      exit();
-
-    } else {
-      
-      $data = array("erro" => true, "mensagem" => "Falha ao salvar usuário");
-      header("Content-Type: application/json");
-      echo json_encode($data);
-      exit();
-    }
-
-  }
-
 }
-
 
 ?>

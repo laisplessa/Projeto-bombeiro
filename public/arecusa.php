3009630a0af6a76561bcc264a1f866d6f30db48d
Maihess <maiaraangelii@gmail.com>
2023-10-25 22:52:00 -0300

    atualização 25/10

--- public/arecusa.php
+++ public/arecusa.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Atendimento Recusa</title>
-  <link rel="stylesheet" type="text/css" href="style-atendimentorecusa.css">
+  <link rel="stylesheet" type="text/css" href="arecusa.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
 </head>
 

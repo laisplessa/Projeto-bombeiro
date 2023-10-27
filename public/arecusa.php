cef56c9a550c3919a6271ac79b8ca9cd2cb53683
Maiihess <maiaraangelii@gmail.com>
2023-10-27 11:39:34 -0300

    atualização 27/10

--- public/arecusa.php
+++ public/arecusa.php
@@ -1,22 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Atendimento Recusa</title>
+  <title>Admin/Login</title>
   <link rel="stylesheet" type="text/css" href="arecusa.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-</head>
-
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
-
+</head>
 <body>
 <div class="logo">
             <img src="logobombeiros.png" height="140" width="140" align="right">
         </div> 
+    <div class="text"><b>O que você deseja?</b></div>
     <!--Botão para acessar formulário de atendimento-->
     <div class="botaoatendimento">
       <b><a href="ficha.html"> Atendimento </a></b>

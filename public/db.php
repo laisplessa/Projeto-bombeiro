453b2ede67aacc02390f428c065a27d1cf444830
Maihess <maiaraangelii@gmail.com>
2023-11-24 12:20:09 -0300

    atualização

--- public/db.php
+++ public/db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "bombeiros";
+$db = "bombeiro";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $db);
